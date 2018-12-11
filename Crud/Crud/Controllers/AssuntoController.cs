1851f36b0dae4f2eb222941ca0492444594735f2
Ariel Passador <arielpassador@gmail.com>
2018-12-11 14:31:06 -0200

    Crud

---
+++ Crud/Crud/Controllers/AssuntoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Crud.Models;
+
+namespace Crud.Controllers
+{
+    public class AssuntoController : Controller
+    {
+        private EscolaEntities db = new EscolaEntities();
+
+        // GET: Assunto
+        public ActionResult Index()
+        {
+            return View(db.Assunto.ToList());
+        }
+
+        // GET: Assunto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assunto assunto = db.Assunto.Find(id);
+            if (assunto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assunto);
+        }
+
+        // GET: Assunto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Assunto/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AssuntoID,AssuntoInfo")] Assunto assunto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assunto.Add(assunto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(assunto);
+        }
+
+        // GET: Assunto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assunto assunto = db.Assunto.Find(id);
+            if (assunto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assunto);
+        }
+
+        // POST: Assunto/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AssuntoID,AssuntoInfo")] Assunto assunto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assunto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(assunto);
+        }
+
+        // GET: Assunto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assunto assunto = db.Assunto.Find(id);
+            if (assunto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assunto);
+        }
+
+        // POST: Assunto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assunto assunto = db.Assunto.Find(id);
+            db.Assunto.Remove(assunto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
